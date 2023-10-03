8e97b7917ee2013f634d7ec08b085ba6e01fcc91
segundo <ladoritaesta@gmail.com>
2023-10-03 09:42:50 +0200

    update

---
+++ Assets/SCRIPTS/Game Manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject snakeHeadGameObject = new GameObject("Snake Head");
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.Instance.snakeHeadSprite;
+    }
+}
